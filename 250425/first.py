f2243f1fb46e71c4d5f7f125666c8b1595e9e3af
GH-57 <plue05007@gmail.com>
2025-04-25 16:50:24 +0900

    1일차 완료

--- 250425/first.py
+++ 250425/first.py
@@ -10,6 +10,78 @@
 print("김치볶음밥은 음~ 마시쪙쪙")
 print("김아무개 와우와우 김치볶음밥")
 
+print("#            #          #")
+print("#            #          #")
+print("#            #          #")
+print("#            #          #")
+print("##############          ##########")
+print("#            #          #")
+print("#            #          #")
+print("#            #          #")
+print("##############          #")
+
+
+
+print("##############          # ")
+print("#                       # ")
+print("#                       # ")
+print("#                       ####### ")
+print("#                       # ")
+print("#                       # ")
+print("##############          # ")
+
+
+# 변수
+
+# 변수이름 = 값
+
+name = "김치찌개"
+name = "김치볶음밥"
+# 변수이름은 의미있어야 한다!
+
+name = "김치찌개"
+print(name)
+name = "김치볶음밥"
+print(name)
+
+age = 10 # 숫자값
+
+
+test = 10
+
+Test = 20
+
+te_st = 1
+
+# 1test (x)
+# te st = 1 (x) 
+
+One = 1
+Two = 2
+Three = 3
+Four = 4
+Five = 5
+print(One, Two, Three, Four, Five) 
+# print(name,name) # 한줄 출력 변수와 변수 사이는 스페이스(띄움)
+
+# 10 + 10 = 20
+Ten = 10
+Twenty = 20
+print(Ten, "+", Ten, "=", Twenty)
+
+age = 10
+print(10)
+
+PI = 3.14 # 대문자로 이루어진 변수는 상수로 취급!
+PI = 2 # BUT, 파이썬은 변수만 허용하기 때문에 아래서 값이 바뀔 수 있음.
+
+print(PI)
+
+# 타입 체크
+print(type(PI))
+print(type(name))
+
+
 
 
 
