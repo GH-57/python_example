c7846f9850a5f4c64b646a88c409bce72e3641d6
GH-57 <plue05007@gmail.com>
2025-05-15 18:22:09 +0900

    미니 프로젝트

--- 백엔드 스터디/미니 프로젝트/1주차/도서관프로젝트.py
+++ 백엔드 스터디/미니 프로젝트/1주차/도서관프로젝트.py
@@ -22,13 +22,38 @@ def login(self,username,password):  # 로그인 함수
     else:
         print("❌ 아이디 또는 비밀번호가 올바르지 않습니다.❌")
 
-# 1-3 로그아웃
 
+current_user = ["abc123"]
+books = [{"title":"원피스", "rented_by":"대여가능"},{"title":"aa", "rented_by": None}]
 
+class library:
 
+    # def __init__(self):
+    #     self.data1 = data1
 
+    # 1-3 로그아웃
+    def logout(self, current_user):
+        confirm = input("로그아웃 하시겠습니까? [1.네 / 2.아니요]")
 
+        if confirm == "1":
+            print("로그아웃 되었습니다.")
+            current_user.clear() # 현재 사용자 정보 초기화
+            return True
+        elif confirm == "0":
+            print("로그아웃이 취소되었습니다.")
+            return False
+        else:
+            print("잘못된 입력입니다. 0 또는 1을 입력해주세요.")
+            return False
 
+    # 2- 1 도서목록 보기
+    def show_books(self, num):
+        for num,title_book in enumerate(books,1):
+            print(f"{num}. {title_book["title"]} / {title_book["rented_by"]}")
+            
+    
+data1 = library()
+data2 = library()
     
 # 2-2 책 추가
 class Addbook:
