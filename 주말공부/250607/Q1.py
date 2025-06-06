97789ce949990b90e25e39925991707b875601a5
GH-57 <plue05007@gmail.com>
2025-06-07 07:46:04 +0900

    토요 복습

---
+++ 주말공부/250607/Q1.py
@@ -0,0 +1,17 @@
+'''
+문제 1: POST 요청 기초
+이름과 나이를 받아서 간단한 메시지를 반환하는 API
+'''
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# /greet 경로에 POST 요청으로 {"name": "홍길동", "age": 25} 형태의 데이터를 받아서
+# {"message": "안녕하세요, 홍길동님! 나이는 25세이군요."} 를 반환하세요
+
+
+
+
+
+
