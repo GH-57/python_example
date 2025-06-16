7d2696a5e954af9dbce10ca8bf2248d67968edc7
GH-57 <plue05007@gmail.com>
2025-06-16 16:38:13 +0900

    chat앱 장고앱 생성 및 index 뷰 구현

---
+++ 6월/250616/chat/views.py
@@ -0,0 +1,8 @@
+# chat/views.py
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# django view: http 요청을 받아 요청을 처리하는 함수
+# - 항상 1개 인자가 있고, request 를 통해 모든 요청 내역을 조회 가능
+def index(request):
+    return HttpResponse("Hello django")
