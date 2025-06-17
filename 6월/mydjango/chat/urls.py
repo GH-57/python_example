870bfd45aa7e2cc388ac6e2c0b363e4a33205b82
GH-57 <plue05007@gmail.com>
2025-06-17 16:51:53 +0900

    Django

--- 6월/mydjango/chat/urls.py
+++ 6월/mydjango/chat/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     # 이 코드를 저장하시면 개발서버에서 index 함수를 찾지못해 오류가 발생할 것이지만,
     # 아래 chat/views.py 저장 후에는 해당 오류가 사라질 것입니다.
     path("", views.index),
+    # chat/urls에 있는 모든 URL 패턴에 일괄적으로
+    #  chat/ 라는 prefix 주소를 부여하겠다.
+    path("messages/new/", views.chat_message_new),
 ]
