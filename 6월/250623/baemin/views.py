8a15f4076be61e9e9a8df2cb7d474e941a63d5dc
GH-57 <plue05007@gmail.com>
2025-06-23 17:59:57 +0900

    35일차 완료

---
+++ 6월/250623/baemin/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from .models import Shop, Review
+from blog.forms import CommentForm
+from blog.models import Comment
+
+# 최신의 가게 목록 페이지를 보여줄 것이다
+# - 최신의 데이터는 DB안에 있다, 그러니 매번 DB 조회를 할 것이다
+def shop_list(request):
+    # DB에서 baemin_shop 테이블의 모든 레코드를 조회할 준비
+    # (아직 모든 데이터를 가져오지 않았다.)
+    qs = Shop.objects.all() # QuerySet
+
+    return render(request,
+                  template_name="baemin/shop_list.html",
+                  context={
+                    "shop_list": qs,
+                  })
+
+
+# TODO: baemin/shop_list.html 템플릿을 만들어보기, 하얀배경도 ok. chatgpt OK
+
+def shop_detail(request, pk):
+    # DB에서 조회했습니다.
+    shop = Shop.objects.get(pk=pk) # 이 필드명 지정이 좀 더 정확한 네이밍.
+    # shop = Shop.objects.get(id=pk)  # 위와 동일한 동작
+
+    # 전체(모든 Shop) 리뷰 데이터를 가져올 준비.
+    review_qs = Review.objects.all()
+    # 특정 shop의 리뷰 데이터를 가져올 준비 (가져올 범위가 좁혀집니다.)
+    review_qs = review_qs.filter(shop=shop)
+
+    return render(
+        request,
+        template_name="baemin/shop_detail.html",
+        context={"shop": shop, "review_list": review_qs},
+    )
+
+
+# TODO: baemin/shop_detail.html 템플릿을 만들어보기.
+
+from .forms import ReviewForm
+
+def review_new(request, shop_pk):
+    if request.method == "GET":
+        form = ReviewForm()
+
+    else:
+        form = ReviewForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            unsaved_review: Comment = form.save(commit=False)
+            unsaved_review.shop = Shop.objects.get(id=shop_pk) ## 
+            unsaved_review.save()
+
+            shop_url = f"/baemin/{shop_pk}/"
+            return redirect(shop_url)
+
+
+
+
+        # TODO: ...
+
+    return render(
+        request,
+        template_name="baemin/review_form.html",
+        context={"form": form},
+    )
---
+++ 6월/250623/chat/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from chat.models import PuzzleRoom
+
+# form.Form: 입력 HTML Form과의 인터페이스
+#   - 입력 폼 HMTL 생성
+#   - 유저가 입력한 값에 대해서 유효성 검사
+#   - 유효성 검사에 통과한 값들을 정리해서.cleaned_datea 사전으로 제공.
+# models.Model: 데이터베이스와의 인터페이스
+
+# forms.ModelForm
+
+
+
+# 상속 (Inheritance) 문법
+class PuzzleRoomForm(forms.ModelForm):
+    class Meta:
+        model = PuzzleRoom
+        # 지정 모델의 모든 모델 필드 내역을 읽어와서, 폼 필드 구성 하기(자동)
+        fields = "__all__"
+
+
+# if 수정 시에는 레벨만 수정토록 허용하고 싶다.  
+class PuzzleRoomEditForm(forms.ModelForm):
+    class Meta:
+        model = PuzzleRoom
+        # 지정 모델의 지정 필드 내역만 읽어와서, 폼 필드 구성허가 (자동)
+        # - 유저에게 level 필드만 입력 필드 제공, 값을 변경할 수 있다.
+        # - 지정 필드에 대해서만 수정 허용
+        fields = ["level"]
+        
