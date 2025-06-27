a82b241d0611de204762b69e90426a4873be26dc
GH-57 <plue05007@gmail.com>
2025-06-27 16:52:24 +0900

    Django

--- 6월/mydjango/accounts/views.py
+++ 6월/mydjango/accounts/views.py
@@ -1,11 +1,23 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
+from .forms import SignupForm
+
+
+login = LoginView.as_view(
+    template_name="accounts/login_form.html",
+)
+
+logout = LogoutView.as_view()
+
+def profile(request):
+    return render(request, "accounts/profile.html")
+
 
 def signup(request):
     if request.method == "GET":
-        form = UserCreationForm()
+        form = SignupForm()
     else:
-        form = UserCreationForm(data=request.POST, files=request.FILES)
+        form = SignupForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save() # 회원가입 완료! 지정 유저 레코드 DB에 저장
             # 회원가입을 했으니, 로그인 페이지로 이동하는 것이 자연스럽다.
